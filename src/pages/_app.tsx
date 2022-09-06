f29ac6c300870689c30e97b9793a9470b446ad10
Plat <github@p1at.dev>
2022-09-06 15:17:55 +0900

    doc: update readme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
+            <Head>
+                <title>Noexes Code Builder</title>
+            </Head>
             <Component {...pageProps} />
         </ChakraProvider>
     )
