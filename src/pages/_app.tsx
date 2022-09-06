1598c2c6fc261532b4e1ccf99aa9e1383ed1c851
Plat <github@p1at.dev>
2022-09-06 15:32:25 +0900

    chore: ogp update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,26 @@ import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import Head from "next/head"
 
+const siteTitle = "Noexes Code Builder"
+const siteDescription = "Builder cheat code from your Noexes pointer!"
+const siteURL = "https://noexes.p1at.dev"
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
             <Head>
-                <title>Noexes Code Builder</title>
+                <title>{siteTitle}</title>
+                <meta name="description" content={siteDescription} />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={siteURL} />
+                <meta property="og:title" content={siteTitle} />
+                <meta property="og:description" content={siteDescription} />
+                <meta property="og:image" content={`${siteURL}/ogp.png`} />
+
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@p1atdev" />
+                <meta name="twitter:creator" content="@p1atdev" />
             </Head>
             <Component {...pageProps} />
         </ChakraProvider>
